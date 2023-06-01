681570f3844a69693aaedbc804eb49f9ec4365ff
ma_shi_chen <1934202608@qq.com>
2023-06-01 15:16:51 +0800

    fix: if name not in blogs return 404

--- src/app/blog/[name]/page.tsx
+++ src/app/blog/[name]/page.tsx
@@ -2,12 +2,25 @@ import React from "react";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import path from "path";
 import matter from "gray-matter";
+import fs from "fs";
 
 export default async function page(props: { params: { name: string } }) {
     const { name } = props.params;
-    const filePath = path.join(process.cwd(), "src", "blogs", `${name}.mdx`);
-    const file = matter.read(filePath);
-    const content = await MDXRemote({ source: file.content });
 
-    return <div className="markdown-body">{content}</div>;
+    const blogsPath = path.join(process.cwd(), "src", "blogs");
+    const files = fs.readdirSync(blogsPath);
+    if (files.includes(`${name}.mdx`)) {
+        const filePath = path.join(
+            process.cwd(),
+            "src",
+            "blogs",
+            `${name}.mdx`
+        );
+        const file = matter.read(filePath);
+        const content = await MDXRemote({ source: file.content });
+
+        return <div className="markdown-body">{content}</div>;
+    } else {
+        return <h1 className="w-full mt-16 text-center text-2xl">404</h1>;
+    }
 }
