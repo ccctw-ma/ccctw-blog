01438403d36afe90c4376a7b031e35c3da1aca15
ma_shi_chen <1934202608@qq.com>
2023-06-01 16:46:25 +0800

    fix: add generateStaticParams to solve dynamic route not working

--- src/app/blog/[name]/page.tsx
+++ src/app/blog/[name]/page.tsx
@@ -7,20 +7,17 @@ import fs from "fs";
 export default async function page(props: { params: { name: string } }) {
     const { name } = props.params;
 
+    const filePath = path.join(process.cwd(), "src", "blogs", `${name}.mdx`);
+    const file = matter.read(filePath);
+    const content = await MDXRemote({ source: file.content });
+
+    return <div className="markdown-body">{content}</div>;
+}
+
+export async function generateStaticParams() {
     const blogsPath = path.join(process.cwd(), "src", "blogs");
     const files = fs.readdirSync(blogsPath);
-    if (files.includes(`${name}.mdx`)) {
-        const filePath = path.join(
-            process.cwd(),
-            "src",
-            "blogs",
-            `${name}.mdx`
-        );
-        const file = matter.read(filePath);
-        const content = await MDXRemote({ source: file.content });
-
-        return <div className="markdown-body">{content}</div>;
-    } else {
-        return <h1 className="w-full mt-16 text-center text-2xl">404</h1>;
-    }
+    return files.map((file) => {
+        name: file.split(".")[0];
+    });
 }
